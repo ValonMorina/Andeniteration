d7177b1ec962e0013ed061bd2f798257711f4923
Valon Morina <69115200+ValonMorina@users.noreply.github.com>
2020-11-24 11:05:43 +0100

    3

---
+++ GitBrugeroverflade/src/sample/Item.java
@@ -0,0 +1,16 @@
+package sample;
+
+
+public class Item {
+
+    String description;
+
+    public Item (String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
